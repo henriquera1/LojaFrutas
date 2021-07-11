fb814474945873dc0f6478a550d88c690c23c711
Henrique Garcia <49287349+henriquera1@users.noreply.github.com>
2021-07-11 19:44:15 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ namespace Frutas
 
                         Console.WriteLine($"\nFruta escolhida: Laranja\nQuantidade: {quantidade}");
                         Console.WriteLine($"Valor total: {quantidade*4},00R$");
-                        Console.WriteLine("Volte Sempre!!");
+                        Console.WriteLine("Volte Sempre!!!");
                     }
 
                     else if (escolhaFruta == 2)
@@ -86,6 +86,19 @@ namespace Frutas
                 case menu.Listar:
                     Console.WriteLine("\nEstoque de frutas disponiveis: \n");
                     Console.WriteLine("Laranja\nBanana\nMelancia\nUva");
+
+                    Console.WriteLine("\nDeseja voltar para o Menu principal?\n");
+                    Console.WriteLine("Digite 'sim' para voltar ao menu.\n");
+                    string voltarMenu = Console.ReadLine();
+                    
+                    if(voltarMenu == "sim")
+                    {
+                        BackMenu();
+                    }
+                    else
+                    {
+                        break;
+                    }
                     break;
                 
                 case menu.Sair:
@@ -97,19 +110,103 @@ namespace Frutas
                     break;
             }
 
-            
-
-            
-           
-
-
-
-
-
-
-
 
                     Console.ReadLine();
         }
+        
+        static public void BackMenu()
+        {
+
+            int quantidade, escolhaFruta;
+
+            Console.WriteLine("\nHORTIFRUTI\n");
+            Console.WriteLine("Selecione uma das opcoes abaixo: ");
+            Console.WriteLine("1-Comprar Frutas\n2-Listar\n3-Sair\n");
+            int escolha = int.Parse(Console.ReadLine());
+
+            menu Opcao = (menu)escolha;
+
+
+
+            switch (Opcao)
+            {
+                case menu.Comprar:
+                    Console.WriteLine("\nSelecione a fruta desejada abaixo: ");
+                    Console.WriteLine("1-Laranja\n2-Banana\n3-Melancia\n4-Uva\n");
+                    escolhaFruta = int.Parse(Console.ReadLine());
+
+                    if (escolhaFruta == 1)
+                    {
+
+                        Console.WriteLine("Informe a quantidade: ");
+                        quantidade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"\nFruta escolhida: Laranja\nQuantidade: {quantidade}");
+                        Console.WriteLine($"Valor total: {quantidade * 4},00R$");
+                        Console.WriteLine("Volte Sempre!!!");
+                    }
+
+                    else if (escolhaFruta == 2)
+                    {
+
+                        Console.WriteLine("Informe a quantidade: ");
+                        quantidade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"\nFruta escolhida: Banana\nQuantidade: {quantidade}");
+                        Console.WriteLine($"Valor total: {quantidade * 2},00R$");
+                        Console.WriteLine("Volte Sempre!");
+                    }
+
+                    else if (escolhaFruta == 3)
+                    {
+
+                        Console.WriteLine("Informe a quantidade: ");
+                        quantidade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"\nFruta escolhida: Melancia\nQuantidade: {quantidade}");
+                        Console.WriteLine($"Valor total: {quantidade * 7},00R$");
+                        Console.WriteLine("Volte Sempre!");
+                    }
+
+                    else if (escolhaFruta == 4)
+                    {
+
+                        Console.WriteLine("Informe a quantidade: ");
+                        quantidade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"\nFruta escolhida: Uva\nQuantidade: {quantidade}");
+                        Console.WriteLine($"Valor total: {quantidade * 3},00R$");
+                        Console.WriteLine("Volte Sempre!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Escolha uma fruta no estoque.");
+                    }
+
+                    break;
+
+                case menu.Listar:
+                    Console.WriteLine("\nEstoque de frutas disponiveis: \n");
+                    Console.WriteLine("Laranja\nBanana\nMelancia\nUva");
+                    break;
+
+                case menu.Sair:
+                    Console.WriteLine("Ate breve!");
+                    break;
+
+                default:
+                    Console.WriteLine("Insira uma opcao valida");
+                    break;
+            }
+
+
+            Console.ReadLine();
+        }
+    
+        
+    
+    
+    
     }
 }
